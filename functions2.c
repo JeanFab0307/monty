d9c6b2b077be59cff19387987d951813689e9ff1
JeanFab0307 <jfrandriatsimivony@gmail.com>
2023-06-21 08:21:24 -0700

    Remove FILE variables from functions2.c

---
+++ functions2.c
@@ -0,0 +1,48 @@
+#include "monty.h"
+
+/**
+ * mul -  multiplies the second top element by the top.
+ * @stack: pointer to stack
+ * @line_number: line_num
+ *
+ * Return: none
+ */
+
+void mul(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+		free_stack(*stack);
+		fclose(file);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n *= (*stack)->n;
+	pop(stack, line_number, file);
+}
+
+/**
+ * mod - mod of second top element with top
+ * @stack: pointer to stack
+ * @line_number: line num
+ *
+ * Return: none
+ */
+
+void mod(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n %= (*stack)->n;
+	pop(stack, line_number, file);
+}
