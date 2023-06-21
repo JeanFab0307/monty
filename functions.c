a845ddadec8f7a26a18ce8ceade6ef5e7edea781
kaleabendrias <kaleabendrias1212@gmail.com>
2023-06-21 02:15:17 -0700

    repaired error msg

--- functions.c
+++ functions.c
@@ -17,7 +17,7 @@ void push(stack_t **stack, unsigned int line_number, FILE *file)
 	arg = strtok(NULL, " ");
 	if (arg == NULL)
 	{
-		fprintf(stderr, "Error: L%u: usage: push integer\n", line_number);
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	/* chek if it is num */
