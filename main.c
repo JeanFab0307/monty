426e2ae8136584d1e4b3227d21390aca8cf7eeab
kaleabendrias <kaleabendrias1212@gmail.com>
2023-06-20 23:40:03 -0700

    fixed usage msg

--- main.c
+++ main.c
@@ -48,8 +48,8 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "Usage: %s file\n", argv[0]);
-		return (EXIT_FAILURE);
+		fprintf(stderr, "Usage: monty file\n");
+		exit(EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
 	if (file == NULL)
